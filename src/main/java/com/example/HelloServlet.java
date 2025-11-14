9438f757a191589e89f0cacc8b14ac0c1177746c
prathmesh-ghatmal <mrjpg2311@gmail.com>
2025-11-14 09:55:42 +0530

    initial commit

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, World from Servlet!</h1>");
+    }
+}
